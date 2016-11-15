0391ceff663ccb3f55690bb3cb06778c9166ffd2
Sashakin <sashawilde@Sashas-MacBook.local>
2016-11-15 11:44:01 +0000

    Booking form and gallery stuff

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,3 +2,26 @@ function select1change() {
   document.getElementById("body").style.backgroundImage =
     "url("+document.getElementById("select1").value+")";
 }
+
+
+<script>
+document.getElementById('links').onclick = function (event) {
+    event = event || window.event;
+    var target = event.target || event.srcElement,
+        link = target.src ? target.parentNode : target,
+        options = {index: link, event: event},
+        links = this.getElementsByTagName('a');
+    blueimp.Gallery(links, options);
+};
+</script>
+
+<script>
+blueimp.Gallery(
+    document.getElementById('links').getElementsByTagName('a'),
+    {
+        container: '#blueimp-gallery-carousel',
+        carousel: true
+    }
+);
+</script>
+
