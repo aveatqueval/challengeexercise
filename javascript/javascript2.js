072f3249df80e89301f4ca469002bbd9cb691914
aveatqueval <aveatquevalentina@gmail.com>
2016-11-16 17:21:40 +0000

    Added one javascript file back on

---
+++ javascript/javascript2.js
@@ -0,0 +1,54 @@
+
+/*
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+<<<<<<< HEAD
+
+
+document.getElementById('links').onclick = function (hide) {
+=======
+document.getElementById('links').onclick = function (event) {
+>>>>>>> origin/master
+    event = event || window.event;
+    var target = event.target || event.srcElement,
+        link = target.src ? target.parentNode : target,
+        options = {index: link, event: event},
+        links = this.getElementsByTagName('a');
+    blueimp.Gallery(links, options);
+};
+
+
+<<<<<<< HEAD
+
+
+=======
+>>>>>>> origin/master
+blueimp.Gallery(
+    document.getElementById('links').getElementsByTagName('a'),
+    {
+        container: '#blueimp-gallery-carousel',
+        carousel: true
+    }
+);
+<<<<<<< HEAD
+
+
+
+
+
+    
+=======
+>>>>>>> origin/master
